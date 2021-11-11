56bb0762cd9ab1d3983a4d44be600614e8d93f15
Alva Melkerson <alva.melkerson@gmail.com>
2021-11-11 08:54:46 -0600

    screen rendering is broken, i am stupid.

--- Makefile
+++ Makefile
@@ -1,6 +1,13 @@
 cligame: main.cpp
 	g++ -o cligame main.cpp
+
+debug: main.cpp
+	g++ -Ddeug -o cligameDEBUG main.cpp -g
+
 test: cligame
 	./cligame
+
+.PHONY: clean
 clean:
 	rm ./cligame
+	rm ./cligameDEBUG
--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <cstring>
 
-#define WIDTH 80 
-#define HEIGHT 24 
+#define WIDTH 4
+#define HEIGHT 2 
 #define PRINT(bar) std::cout << bar << std::endl
 #define DEBUGP(foo) PRINT("At coordinates (" + std::to_string(x) + ", " + std::to_string(y) + ") Value is: " + std::to_string(foo))
 
 namespace game {
-	bool running = true;
-	void renderGameScreen() {
-		char screen[HEIGHT][WIDTH+1];
+	char screen[WIDTH][HEIGHT]{};
+	bool running;
+	void drawFrameBuffer() {
 //		char digits[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-		for (int y = 0; y < HEIGHT; ++y) {
+		for (int y = 0; y < HEIGHT; y++) {
 			for (int x = 0; x < WIDTH+1; x++) {
 				if (x == WIDTH) {
 					screen[y][x] = '\0';
@@ -23,12 +23,13 @@ namespace game {
 				}
 			}
 		}
-		for (int y = 0; y < HEIGHT; ++y) {
-			PRINT(screen[y]);	
-		}
 	}
 
 	void getInput() {
+		char n{};
+		std::cout << "INPUT: ";
+		n = std::getchar();
+		PRINT(n);
 		return;
 	}
 
@@ -37,11 +38,20 @@ namespace game {
 		return;
 	}
 
+	void refresh() {
+		for(int y = 0; y < HEIGHT; y++) {
+//			PRINT(y);
+			PRINT(screen[y]);
+		}
+	}
+
 	void mainLoop() {
+		running = true;
 		while (running) {
-			renderGameScreen();
-			getInput();
+			drawFrameBuffer();
+//			getInput();
 			updateGameState();
+			refresh();
 		}
 	}
 
